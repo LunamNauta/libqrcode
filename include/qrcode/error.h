344b9577c281967d0692070c61c698a3caec9493
Ayden Kranz <ayden.kranz@gmail.com>
2026-02-18 14:43:55 -0500

    Added alphanumeric and byte encode

--- include/qrcode/error.h
+++ include/qrcode/error.h
@@ -9,6 +9,7 @@
 #define QRCODE_ERROR_MODE          (8)
 #define QRCODE_ERROR_ENCODING_MODE (QRCODE_ERROR_ENCODING | QRCODE_ERROR_MODE)
 #define QRCODE_ERROR_ECL           (16)
+#define QRCODE_ERROR_DATA          (32)
 
 #define QRCODE_ERROR(err, ret) (errno = (err), ret)
 
--- src/qrcode/encoding.c
+++ src/qrcode/encoding.c
@@ -5,11 +5,33 @@
 #include "qrcode/priv/common.h"
 #include "qrcode/validation.h"
 #include "qrcode/common.h"
+#include "qrcode/error.h"
+
+int qrcode_alphanumeric_value(uint8_t ch){
+    if (ch >= '0' && ch <= '9') return ch - '0';
+    if (ch >= 'A' && ch <= 'Z') return ch - 'A' + 10;
+    switch (ch){
+        case ' ': return 36;
+        case '$': return 37;
+        case '%': return 38;
+        case '*': return 39;
+        case '+': return 40;
+        case '-': return 41;
+        case '.': return 42;
+        case '/': return 43;
+        case ':': return 44;
+    }
+    return -1;
+}
 
 int qrcode_encode_numeric(uint8_t* buf, const uint8_t* data, size_t dlen, int ver, int ecl, size_t* ref_bit){
     struct qrcode_capacity_info cinfo;
     struct qrcode_encoding_info einfo;
     size_t bits;
+
+    for (size_t a = 0; a < dlen; a++){
+        if (data[a] < '0' || data[a] > '9') return QRCODE_ERROR(QRCODE_ERROR_DATA, -1);
+    }
 
     if (qrcode_capacity_info(ver, ecl, QRCODE_ENCODING_NUMERIC, &cinfo) < 0) return -1;
     if (qrcode_encoding_info(ver, QRCODE_ENCODING_NUMERIC, &einfo) < 0) return -1;
@@ -44,8 +66,66 @@ int qrcode_encode_numeric(uint8_t* buf, const uint8_t* data, size_t dlen, int ve
 
     return 0;
 }
-int qrcode_encode_alphanumeric(uint8_t* buf, const uint8_t* data, size_t dlen, int ver, int ecl, size_t* ref_bit);
-int qrcode_encode_byte(uint8_t* buf, const uint8_t* data, size_t dlen, int ver, int ecl, size_t* ref_bit);
+int qrcode_encode_alphanumeric(uint8_t* buf, const uint8_t* data, size_t dlen, int ver, int ecl, size_t* ref_bit){
+    struct qrcode_capacity_info cinfo;
+    struct qrcode_encoding_info einfo;
+    size_t bits;
+
+    for (size_t a = 0; a < dlen; a++){
+        if (qrcode_alphanumeric_value(data[a]) < 0) return QRCODE_ERROR(QRCODE_ERROR_DATA, -1);
+    }
+
+    if (qrcode_capacity_info(ver, ecl, QRCODE_ENCODING_ALPHANUMERIC, &cinfo) < 0) return -1;
+    if (qrcode_encoding_info(ver, QRCODE_ENCODING_ALPHANUMERIC, &einfo) < 0) return -1;
+    bits = cinfo.codewords*8 - cinfo.truncated*4;
+
+    qrcode_write_bits(buf, einfo.mode, einfo.mode_len, ref_bit);
+    qrcode_write_bits(buf, dlen, einfo.count_len, ref_bit);
+
+    for (size_t a = 0; a < dlen;){
+        size_t rem = dlen - a;
+        uint16_t val;
+        size_t nbits;
+        if (rem >= 2){
+            val = qrcode_alphanumeric_value(data[a])*45 + qrcode_alphanumeric_value(data[a + 1]);
+            nbits = 11;
+            a += 2;
+        }
+        else if (rem == 1){
+            val = qrcode_alphanumeric_value(data[a]);
+            nbits = 6;
+            a += 1;
+        }
+        else break;
+        if (*ref_bit + nbits > bits) return -1;
+        qrcode_write_bits(buf, val, nbits, ref_bit);
+    }
+
+    return 0;
+}
+int qrcode_encode_byte(uint8_t* buf, const uint8_t* data, size_t dlen, int ver, int ecl, size_t* ref_bit){
+    struct qrcode_capacity_info cinfo;
+    struct qrcode_encoding_info einfo;
+    size_t bits;
+
+    for (size_t a = 0; a < dlen; a++){
+        if (data[a] > 0x7f) return QRCODE_ERROR(QRCODE_ERROR_DATA, -1);
+    }
+
+    if (qrcode_capacity_info(ver, ecl, QRCODE_ENCODING_ALPHANUMERIC, &cinfo) < 0) return -1;
+    if (qrcode_encoding_info(ver, QRCODE_ENCODING_ALPHANUMERIC, &einfo) < 0) return -1;
+    bits = cinfo.codewords*8 - cinfo.truncated*4;
+
+    qrcode_write_bits(buf, einfo.mode, einfo.mode_len, ref_bit);
+    qrcode_write_bits(buf, dlen, einfo.count_len, ref_bit);
+
+    for (size_t a = 0; a < dlen;){
+        if (*ref_bit + 8 > bits) return -1;
+        qrcode_write_bits(buf, data[a], 8, ref_bit);
+    }
+
+    return 0;
+}
 int qrcode_encode_kanji(uint8_t* buf, const uint8_t* data, size_t dlen, int ver, int ecl, size_t* ref_bit);
 
 int qrcode_append_terminator(uint8_t* buf, int ver, int ecl, size_t* ref_bit){
